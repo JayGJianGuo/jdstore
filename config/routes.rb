1bd53dea68bd422e7a4484bac282afc1d75e1047
jay.g <jkland927@hotmail.com>
2017-02-09 16:06:57 +0800

    debug in checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :orders
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
@@ -20,7 +22,5 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
-
-  resources :orders
 
 end
