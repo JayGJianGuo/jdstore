a55415365b4a24845448a7a5ccfa9c578f4305d5
jay.g <jkland927@hotmail.com>
2017-02-18 20:44:03 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
       post :pay_with_wechat
     end
   end
-  
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   namespace :admin do
     resources :products
+    resources :orders
   end
 
   resources :products do
